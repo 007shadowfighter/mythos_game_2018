4568bc3cc448c69cd8dc9bd7d3b8da0fed37854a
007shadowfighter <007shadowfighter@gmail.com>
2018-03-14 17:04:24 -0700

    first commit

---
+++ Assets/scripts/hero.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hero : MonoBehaviour {
+    private Rigidbody2D myRigidbody;
+
+    [SerializeField]
+    private float movementspeed;
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void fixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+       
+       
+            HandleMovement(horizontal);
+    }
+    private void HandleMovement(float horizontal)
+    {
+
+        myRigidbody.velocity = new Vector2(horizontal * movementspeed, myRigidbody.velocity.y);
+    }
+    
+}
