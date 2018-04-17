6c6a9fa217082210846b104f6d62f53cd30a587c
007shadowfighter <007shadowfighter@gmail.com>
2018-04-17 14:21:07 -0700

    enemy flips on x
    changes to move script
    flip added
    level ends
    start screen changes

---
+++ Assets/2d Pixel environment/Randomizer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Randomizer : MonoBehaviour {
+
+    public SpriteRenderer SpriteToRandom;
+    public Sprite[] _SpriteImage;
+
+    void Start()
+    {
+        SpriteToRandom = GetComponent<SpriteRenderer>();
+        Randomize();
+    }
+
+    public void Randomize()
+    {
+        SpriteToRandom.sprite = _SpriteImage[Random.Range(0, _SpriteImage.Length)];
+    }
+}
