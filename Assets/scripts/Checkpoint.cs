27d283e814a045a8ce8bbd3581acbe29c33f1f20
007shadowfighter <007shadowfighter@gmail.com>
2018-04-27 13:41:23 -0700

    more changes

---
+++ Assets/scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+    public Vector3 respawnPoint;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "fallDetector") {
+            transform.position = respawnPoint;
+            if (other.tag == "Checkpoint")
+            {
+                respawnPoint = other.transform.position;
+            }
+        }
+    }
+}
+        
+    
+
---
+++ Assets/scripts/EnemyDamage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroy(collision.gameObject);
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        int damage = 20;
+
+        if (health != null)
+        {
+            health.TakeDamage(damage);
+        }
+    }
+}
